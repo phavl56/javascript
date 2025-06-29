3bb33ec5142250c5e100f48bb999acfd0d0cec77
lichwyy <phavl291106@gmail.com>
2025-06-29 11:15:46 -0300

    uma penca de aula aí, aqui vai até a 11

---
+++ aulas/aula11/ex008.js
@@ -0,0 +1,8 @@
+var velocidade = 60
+console.log(`A velocidade do seu carro é: ${velocidade}`)
+
+if (velocidade > 60){ 
+    console.log(`Você ultrapassou a velocidade permitida`)
+}
+
+console.log(`Dirija sempre usando cinto de segurança`)
---
+++ aulas/aula11/ex009.js
@@ -0,0 +1,10 @@
+var country = 'Brasil'
+
+console.log(`Nascido em ${country}`)
+
+if (country == 'Brasil') {
+    console.log("Você é brasileiro!")
+}
+else{
+    console.log("Você é estrangeiro!")
+}
